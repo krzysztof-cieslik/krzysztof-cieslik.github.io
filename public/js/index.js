7d97d3013013b6186b7e0ca3371583e4b805b624
krzysztof-cieslik <mrkrzysiex@gmail.com>
2021-09-22 20:34:05 +0200

    modyfikacja tego co mazurek wcale nie zepsul

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,22 @@
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    const games = ["minecraft", "csgo", "brawlstars", "amogus", "fortnite", "roblox"] 
+document.addEventListener("DOMContentLoaded", () => {
+    const games = ["minecraft", "csgo", "brawlstars", "amogus", "fortnite", "roblox"]
 
     games.map((game) => {
         const element = document.getElementById(`${game}_case`)
 
         const body = document.getElementsByTagName("body")[0]
-       
-        element.addEventListener("mouseenter", (event) => {
 
-            body.animate({ backgroundImage: `url(../assets/backgrounds/bg_${game}.png)` }, {
+        element.addEventListener("mouseenter", () => {
+
+            body.animate({backgroundImage: `url(../assets/backgrounds/bg_${game}.png)`}, {
                 duration: 500, fill: "forwards"
             })
+        })
+        element.addEventListener("mouseleave", () => {
 
-           
-  
+            body.animate({backgroundImage: `none`, backgroundColor: '#1a1a1a'}, {
+                duration: 500, fill: "forwards", delay: 1000
+            })
         })
     })
 
