4f2946242fa1c528b80aed3f7096b34a14ef8941
krzysztof-cieslik <mrkrzysiex@gmail.com>
2021-09-22 22:12:42 +0200

    fajne animacje dodalem

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const body = document.getElementsByTagName("body")[0]
 
-        element.addEventListener("mouseenter", () => {
+        const charLeft = document.getElementsByClassName("char-left")[0]
+        const charRight = document.getElementsByClassName("char-right")[0]
 
+        element.addEventListener("mouseenter", () => {
             body.animate({backgroundImage: `url(../assets/backgrounds/bg_${game}.png)`}, {
                 duration: 500, fill: "forwards"
             })
+            charLeft.animate({backgroundImage: `url(../assets/characters/char_${game}_1.png`}, {
+                duration: 500, fill: "forwards"
+            })
+            charRight.animate({backgroundImage: `url(../assets/characters/char_${game}_2.png`}, {
+                duration: 500, fill: "forwards"
+            })
         })
-       
     })
-
-});
+})
